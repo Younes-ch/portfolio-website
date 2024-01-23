88f3536cc48b8aef9f10984a3438f8611741d7c8
Younes-ch <younes.jmell10@gmail.com>
2024-01-23 02:45:48 +0100

    Update Resume.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (chunkInfo) => {
+          if (chunkInfo.name.indexOf("Resume") >= 0) {
+            return "Younes' Resume.pdf"
+          }
+          return chunkInfo.name;
+        },
+      },
+    },
+  }
 })
